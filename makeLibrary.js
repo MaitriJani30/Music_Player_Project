28ff3dfad27cdf978eee2c4702f55cca8e57b3da
Prernapatel2301 <141484784+Prernapatel2301@users.noreply.github.com>
2023-09-03 18:57:01 +0530

    Update makeLibrary.js

--- makeLibrary.js
+++ makeLibrary.js
@@ -1,60 +1,75 @@
-function addMore(){
-    document.getElementById('error').innerHTML = "";
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Varela+Round&display=swap');
+:root{
+    --white : white;
+    --black : black;
+    --silver :  rgb(27, 27, 27);
+}
+body{
+    background-color: var(--black);
+}
+.row{
+    /* background-color: aliceblue; */
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    padding: 20px 30px;
+}
+.row input{
 
-    let name = document.getElementById('name').value;
-    if(name==''){
-        document.getElementById('error').innerHTML = "enter value";
-    }
-    else{
-        let box = document.getElementById('box');
-
-        let li = document.createElement('li');
-        li.textContent = name;
-
-        let a = document.createElement('a');
-        a.textContent = 'x';
-        a.href = "javascript:void(0)";
-        a.className = "remove";
-        li.appendChild(a);
-        let pos = box.firstElementChild;
-
-        if(pos==null){    
-            box.appendChild(li);
-        }
-        else{
-            box.insertBefore(li , pos);
-        }
-    }
-    saveData();
-    document.getElementById('name').value="";
-
+    padding: 10px;
+    border-radius: 8px;
+    border: none;
+    outline: none;
+}
+.row #item{
+    padding-right: 450px;
+      border-top-right-radius: 0px;
+      border-bottom-right-radius: 0px;
+}
+.row #submit{
+    color: var(--white);
+    background-color: #7c8184;
+    font-size: 14px;
+    border-top-left-radius: 0px;
+    border-bottom-left-radius: 0px;
+}
+ul{
+    list-style-type: none;
+}
+#addForm{
+    position: relative;
+}
+#addForm ul{
+    position: absolute;
+    top: 25%;
+    right:0%;
+}
+#addForm ul li{
+ padding: 0px 10px 30px 0px;
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content:space-around;
 }
 
-let btn = document.querySelector('ul');
-btn.addEventListener('click' , function(e){
-    let box = document.getElementById('box');
-
-    let li =e.target.parentNode;
-    box.removeChild(li);
-    saveData();
-});
-
-
-
-
-
-
-
-
-//  saving data
-
-function saveData(){
-    localStorage.setItem("data" , box.innerHTML);
+.btn-danger{
+background-color:red;
+border: none;
+outline: none;
+margin-left: 20px;
+}
+.btn-danger:hover{
+    background-color:#d9534f;
 }
 
-function show(){
-    box.innerHTML =  localStorage.getItem("data");
+.edit{
+    background-color:#5cb85c;
+    border: none;
+    outline: none;
 }
-show();
-
-
+.edit:hover{
+    background-color:#5cb85c;
+}
+ 
